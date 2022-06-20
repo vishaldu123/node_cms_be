0340f619bf401e75932ae2ed1749bdd8f1d47160
vishal <vishal@keensolution.in>
2022-06-20 23:19:28 +0530

    init

--- routes/pages.js
+++ routes/pages.js
@@ -7,20 +7,20 @@ Router.get('/',(req,res,next)=>{
        if(!err){res.send(rows);}else{console.log("err:",err);} 
     })
 })
-Router.get('/:id', (req, res,next)=>{
+Router.get('/p/:slug', (req, res,next)=>{
+    mysqlConection.query('SELECT * FROM pages WHERE slug = "'+req.params.slug+'" ',(err,rows,fields)=>{
+        //console.log("here----",req.params.slug);
+        if(!err){res.send(rows);}else{console.log("err:",err);} 
+     })
+})
+Router.get('/id/:id', (req, res,next)=>{
     mysqlConection.query('SELECT * FROM pages WHERE id = ' + req.params.id,(err,rows,fields)=>{
         if(!err){res.send(rows);}else{console.log("err:",err);} 
      })
 })
-
-Router.get('/:page', (req, res,next)=>{
-    mysqlConection.query('SELECT * FROM pages WHERE slug = ' + req.params.slug,(err,rows,fields)=>{
-        if(!err){res.send(rows);}else{console.log("err:",err);} 
-     })
-})
-
 Router.get('/home', (req, res,next)=>{
     mysqlConection.query('SELECT * FROM home_page order by position_order',(err,rows,fields)=>{
+         console.log("here----","home");
         if(!err){res.send(rows);}else{console.log("err:",err);} 
      })
 })
